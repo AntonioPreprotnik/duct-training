d98b1dc1a96fe62146911c730473bda24d2477ba
TheGamePrep <preprotnik.novi@gmail.com>
2021-12-29 14:30:33 +0100

    Tutorials one and two

---
+++ src/sms/handler/api.clj
@@ -0,0 +1,12 @@
+(ns sms.handler.api
+  (:require [compojure.core :as compojure]
+            [integrant.core :as ig]
+            [sms.handler.api.message :as message]))
+
+(defmethod ig/init-key :sms.handler/api 
+  [_ {:keys [message-service]}]
+  (compojure/context "/messages" []
+    (compojure/POST "/" [:as req]
+      (message/send! message-service (:body-params req)))))
+
+
